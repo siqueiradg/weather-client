89a2e46a0bbe83a757230557904ffe76edd0abf2
Douglas Siqueira <douglas.siqueira@nastek.com.br>
2018-03-11 16:50:47 -0400

    Criado componente e serviço Weather

---
+++ src/app/weather/weather.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather',
+  templateUrl: 'weather.component.html'
+})
+
+export class WeatherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { }
+}
